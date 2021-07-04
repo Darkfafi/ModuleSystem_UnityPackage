192afee1cf37c6ca5a228fe74df000199a5e8188
Ramses Di Perna <ramses@sparklingsociety.net>
2021-07-04 19:48:37 +0200

    Implemented ModuleSystem package 1.0.0

---
+++ IModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ModuleSystem.Core
+{
+	public interface IModule
+	{
+		string UniqueIdentifier
+		{
+			get;
+		}
+
+		bool TryProcess(ModuleAction action, Action unlockMethod);
+		void OnResolvedStack(ModuleAction coreAction);
+		void Init(ModuleProcessor parent);
+		void StartModule();
+		void Deinit();
+	}
+}
