3a41ebf460514ffdfbfaaa9201b1ac555b44d81b
Ramses <ik_ben_ramses@hotmail.com>
2022-03-06 11:16:44 +0100

    Made it so Lambda Modules also have multi processing support

--- DelayedLambdaModule.cs
+++ DelayedLambdaModule.cs
@@ -7,13 +7,47 @@ namespace ModuleSystem
 		public delegate bool ModuleHandler(ModuleAction action, Action unlockMethod, ModuleProcessor parent);
 
 		private ModuleHandler _handler;
+		private bool _multiProcessing = false;
 
-		public DelayedLambdaModule(ModuleHandler handler)
+		public override bool AllowMultiProcessing => _multiProcessing;
+
+
+		public DelayedLambdaModule(ModuleHandler handler, bool multiProcessing = false)
 		{
 			_handler = handler;
+			_multiProcessing = multiProcessing;
 		}
 
 		protected override bool TryProcessInternal(ModuleAction action, Action unlockMethod)
+		{
+			return _handler(action, unlockMethod, Processor);
+		}
+
+		public override void Deinit()
+		{
+			_handler = null;
+			base.Deinit();
+		}
+	}
+
+	public class DelayedLambdaModule<T> : DelayedModuleBase<T>
+		where T : ModuleAction
+	{
+		public delegate bool ModuleHandler(T action, Action unlockMethod, ModuleProcessor parent);
+
+		private ModuleHandler _handler;
+		private bool _multiProcessing = false;
+
+		public override bool AllowMultiProcessing => _multiProcessing;
+
+
+		public DelayedLambdaModule(ModuleHandler handler, bool multiProcessing = false)
+		{
+			_handler = handler;
+			_multiProcessing = multiProcessing;
+		}
+
+		protected override bool TryProcessInternal(T action, Action unlockMethod)
 		{
 			return _handler(action, unlockMethod, Processor);
 		}
